e1a7ef0f564ea09651b19bce0c24af3cd7080371
sereshkash <sereshkash@yandex.ru>
2024-04-04 10:57:52 +0300

    add conv FASTA and Muscle MSA

--- seq_split.py
+++ seq_split.py
@@ -1,4 +1,5 @@
 import random
+import subprocess
 
 def number_split(len_s, N, len_subsequence):
     beginnings = [0] * N
@@ -16,5 +17,29 @@ def number_split(len_s, N, len_subsequence):
 
 def string_split(S, N, len_subsequence):
     beginnings = number_split(len(S), N, len_subsequence)
-    set_of_beginnings = [S[i:i + len_subsequence] for i in beginnings]
-    return set_of_beginnings
+    set_of_subseq = [S[i:i + len_subsequence] for i in beginnings]
+    return set_of_subseq
+
+def creating_fasta_set(set_of_subseq, name = 'example.fa'):
+    string_set_of_subset = ''
+    for i in range(len(set_of_subseq)):
+        string_set_of_subset += '>i ' + str(i + 1) + ';\n' +str(set_of_subseq[i]) + '\n'
+    file = open(name,'w')
+    file.write(string_set_of_subset)
+    file.close()
+
+def muscle_msa(name_in, name_out = 'seqs1.afa'):
+    process = subprocess.run(['muscle', '-in', name_in, '-out', name_out], capture_output=True, text = True)
+    return process
+
+
+
+# set = string_split('CACAGGACTGTCGAAAGCAG', 3, 4)
+# name = "example.fa"
+# creating_fasta_set(set, name)
+# muscle_msa(name, "seqs2.afa")
+
+
+
+#creating_fasta_set(['ATTGC', 'AATTGC', 'ATGC'])
+
